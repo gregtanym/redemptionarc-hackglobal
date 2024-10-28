d5e3040487fd8669a696861b0877e100a495eb93
gregtanym <gregtanym@gmail.com>
2024-10-28 16:55:13 -0400

    set default chat id

--- lyf-app/app/Context/store.jsx
+++ lyf-app/app/Context/store.jsx
@@ -6,7 +6,7 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   // insert your states here (pass it into the value attribute at the bottom)
   const [selectedUserId, setSelectedUserId] = useState(1);
-  const [selectedChatId, setSelectedChatId] = useState();
+  const [selectedChatId, setSelectedChatId] = useState(1);
 
   // insert your functions here (pass it into the value attribute at the bottom)
 
